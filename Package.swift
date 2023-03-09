8cf47818a9656857b3305732daf97c5d23c2832c
Alexander Kraev <aleksandr.kraev@lenta.com>
2023-03-09 11:37:18 +0300

    add *.a files to lib in sources

--- Package.swift
+++ Package.swift
@@ -5,20 +5,18 @@ import PackageDescription
 let package = Package(
     name: "CardIO",
     platforms: [
-        .iOS(.v8),
+        .iOS(.v12),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CardIO", targets: ["CardIO"]),
+            name: "CardIO",
+            targets: ["CardIO"]),
     ],
     dependencies: [ ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CardIO",
-            dependencies: []),
-    ],
-    swiftLanguageVersions: [.v5]
+            dependencies: [],
+            linkerSettings: [LinkerSetting.unsafeFlags(["-L./Sources/CardIO/lib"])]),
+    ]
 )
